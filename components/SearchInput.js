0241e1c70c3beb061c95c01376602e528e685ac9
Jonathan <jonbezeau@gmail.com>
2021-10-16 12:27:09 -0700

    Added support for user to choose max results number

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -4,9 +4,7 @@ import arrayContains from '../util/arrayContains';
 export default function SearchInput(props) {
     const { setter } = props;
 
-    const numResults = 5;
-
-    const [searchOjbect, setSearchObject] = useState({
+    const [searchObject, setSearchObject] = useState({
         noun: false,
         verb: false,
         adjective: false,
@@ -16,26 +14,27 @@ export default function SearchInput(props) {
         startsWith: "",
         endsWith: "",
         rymesWith: "",
-        similarTo: ""
+        similarTo: "",
+        numResults: 5
     })
 
     function generateAPICall() {
-        let result = 'https://api.datamuse.com/words?md=dsp&max=900';
-        if (searchOjbect.startsWith.length > 0) {
-            if (searchOjbect.endsWith.length > 0) {
-                result += `&sp=${searchOjbect.startsWith}*${searchOjbect.endsWith}`
+        let result = 'https://api.datamuse.com/words?md=dsp&max=999';
+        if (searchObject.startsWith.length > 0) {
+            if (searchObject.endsWith.length > 0) {
+                result += `&sp=${searchObject.startsWith}*${searchObject.endsWith}`
             }
-            result += `&sp=${searchOjbect.startsWith}*`
+            result += `&sp=${searchObject.startsWith}*`
         } else {
-            if (searchOjbect.endsWith.length > 0) {
-                result += `&sp=*${searchOjbect.endsWith}`
+            if (searchObject.endsWith.length > 0) {
+                result += `&sp=*${searchObject.endsWith}`
             }
         }
-        if (searchOjbect.rymesWith.length > 0) {
-            result += `&rel_rhy=${searchOjbect.rymesWith}`
+        if (searchObject.rymesWith.length > 0) {
+            result += `&rel_rhy=${searchObject.rymesWith}`
         }
-        if (searchOjbect.similarTo.length > 0) {
-            result += `&ml=${searchOjbect.similarTo}`
+        if (searchObject.similarTo.length > 0) {
+            result += `&ml=${searchObject.similarTo}`
         }
         return result;
     }
@@ -43,13 +42,13 @@ export default function SearchInput(props) {
     function stripData(dataWords) {
         let strippedData = [];
         for (let dataWord of dataWords) {
-            if (strippedData.length >= numResults) break;
-            if (!searchOjbect.noun && arrayContains(dataWord.tags, 'n')) continue;
-            if (!searchOjbect.verb && arrayContains(dataWord.tags, 'v')) continue;
-            if (!searchOjbect.adjective && arrayContains(dataWord.tags, 'adj')) continue;
-            if (!searchOjbect.adverb && arrayContains(dataWord.tags, 'adv')) continue;
-            if (searchOjbect.useSyllables) {
-                if (searchOjbect.syllables != dataWord.numSyllables) continue;
+            if (strippedData.length >= searchObject.numResults) break;
+            if (!searchObject.noun && arrayContains(dataWord.tags, 'n')) continue;
+            if (!searchObject.verb && arrayContains(dataWord.tags, 'v')) continue;
+            if (!searchObject.adjective && arrayContains(dataWord.tags, 'adj')) continue;
+            if (!searchObject.adverb && arrayContains(dataWord.tags, 'adv')) continue;
+            if (searchObject.useSyllables) {
+                if (searchObject.syllables != dataWord.numSyllables) continue;
             }
             strippedData.push(dataWord);
         }
@@ -73,62 +72,68 @@ export default function SearchInput(props) {
     }
 
     function handleNounChange() {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.noun = !result.noun;
         setSearchObject(result);
     }
 
     function handleVerbChange() {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.verb = !result.verb;
         setSearchObject(result);
     }
 
     function handleAdjectiveChange() {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.adjective = !result.adjective;
         setSearchObject(result);
     }
 
     function handleAdverbChange() {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.adverb = !result.adverb;
         setSearchObject(result);
     }
 
     function handleUseSyllablesChange() {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.useSyllables = !result.useSyllables;
         setSearchObject(result);
     }
 
     function handleSyllablesChange(e) {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.syllables = e.target.value ? e.target.value : 1
         setSearchObject(result);
     }
 
     function handleStartsWithChange(e) {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.startsWith = e.target.value ? e.target.value : "";
         setSearchObject(result);
     }
 
     function handleEndsWithChange(e) {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.endsWith = e.target.value ? e.target.value : "";
         setSearchObject(result);
     }
 
     function handleRymesWithChange(e) {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.rymesWith = e.target.value ? e.target.value : "";
         setSearchObject(result);
     }
 
     function handleSimilarToChange(e) {
-        let result = {... searchOjbect};
+        let result = {... searchObject};
         result.similarTo = e.target.value ? e.target.value : "";
+        setSearchObject(result);
+    }
+
+    function handleNumResultsChange(e) {
+        let result = {... searchObject};
+        result.numResults = e.target.value ? e.target.value : 1
         setSearchObject(result);
     }
 
@@ -137,42 +142,46 @@ export default function SearchInput(props) {
             <p>Search area</p>
             <div>
                 <label htmlFor="noun">Nouns</label>
-                <input type="checkbox" id="noun" value={searchOjbect.noun} onChange={handleNounChange}></input>
+                <input type="checkbox" id="noun" value={searchObject.noun} onChange={handleNounChange}></input>
             </div>
             <div>
                 <label htmlFor="verb">Verbs</label>
-                <input type="checkbox" id="verb" value={searchOjbect.verb} onChange={handleVerbChange}></input>
+                <input type="checkbox" id="verb" value={searchObject.verb} onChange={handleVerbChange}></input>
             </div>
             <div>
                 <label htmlFor="adjective">Adjectives</label>
-                <input type="checkbox" id="adjective" value={searchOjbect.adjective} onChange={handleAdjectiveChange}></input>
+                <input type="checkbox" id="adjective" value={searchObject.adjective} onChange={handleAdjectiveChange}></input>
             </div>
             <div>
                 <label htmlFor="adverb">Adverbs</label>
-                <input type="checkbox" id="adverbs" value={searchOjbect.adverb} onChange={handleAdverbChange}></input>
+                <input type="checkbox" id="adverbs" value={searchObject.adverb} onChange={handleAdverbChange}></input>
             </div>
             <div>
                 <label htmlFor="syllable">Syllables: </label>
-                <input type="checkbox" id="useSyllables" value={searchOjbect.useSyllables} onChange={handleUseSyllablesChange}></input>
-                {searchOjbect.useSyllables && (
-                    <input type="number" id="syllables" value={searchOjbect.syllables} onChange={handleSyllablesChange}></input>
+                <input type="checkbox" id="useSyllables" value={searchObject.useSyllables} onChange={handleUseSyllablesChange}></input>
+                {searchObject.useSyllables && (
+                    <input type="number" id="syllables" value={searchObject.syllables} onChange={handleSyllablesChange}></input>
                 )}
             </div>
             <div>
                 <label htmlFor="startsWith">Starts with: </label>
-                <input type="text" id="startsWith" value={searchOjbect.startsWith} onChange={handleStartsWithChange}></input>
+                <input type="text" id="startsWith" value={searchObject.startsWith} onChange={handleStartsWithChange}></input>
             </div>
             <div>
                 <label htmlFor="endsWith">Ends with: </label>
-                <input type="text" id="endsWith" value={searchOjbect.endsWith} onChange={handleEndsWithChange}></input>
+                <input type="text" id="endsWith" value={searchObject.endsWith} onChange={handleEndsWithChange}></input>
             </div>
             <div>
                 <label htmlFor="rymesWith">Rymes with: </label>
-                <input type="text" id="RymesWith" value={searchOjbect.rymesWith} onChange={handleRymesWithChange}></input>
+                <input type="text" id="RymesWith" value={searchObject.rymesWith} onChange={handleRymesWithChange}></input>
             </div>
             <div>
                 <label htmlFor="similarTo">Similar to: </label>
-                <input type="text" id="similarTo" value={searchOjbect.similarTo} onChange={handleSimilarToChange}></input>
+                <input type="text" id="similarTo" value={searchObject.similarTo} onChange={handleSimilarToChange}></input>
+            </div>
+            <div>
+                <label htmlFor="numResults">Number of Results: </label>
+                <input type="number" id="numResults" value={searchObject.numResults} onChange={handleNumResultsChange}></input>
             </div>
             <button onClick={getSearchResults}>Search</button>
         </>
